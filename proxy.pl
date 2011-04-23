796f198264a4dfd4e0f7cc3caa27715bca5474cd
Mark Ethan Trostler <mark@zzo.com>
2011-04-23 14:47:37 -0700

    Handle gzipped docs

--- proxy.pl
+++ proxy.pl
@@ -4,9 +4,12 @@ use HTTP::Proxy qw(:log);
 use HTTP::Proxy::BodyFilter::simple;
 use HTTP::Proxy::BodyFilter::complete;
 use HTTP::Proxy::HeaderFilter::simple;
+use Compress::Zlib;
 use Data::Dumper;
 
-           # a simple User-Agent filter
+my $contentEncoding;
+my $hostname = 'ps48174.dreamhostps.com:8081';
+
 my $INSERT = <<END;
 <script>
 (function(d) {
@@ -19,34 +22,44 @@ my $INSERT = <<END;
 </script>
 END
 
-my $hostname = 'ps48174.dreamhostps.com:8081';
 my $css = '<link rel="stylesheet" href="http://' . $hostname . '/footer.css" type="text/css" />';
 
+my $gzip = Compress::Zlib::memGzip($INSERT);
+my $gzip_css = Compress::Zlib::memGzip($css);
 # init
 my $proxy = HTTP::Proxy->new( port => 8080, host => '', max_clients => 100 );
 $proxy->push_filter(
     mime     => 'text/html',
+    response => HTTP::Proxy::BodyFilter::complete->new,
     response => HTTP::Proxy::BodyFilter::simple->new(
         sub {
-#            my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
-#            ${ $_[1] } =~ s!<head(.*?)>!<head$1>$css!i;
-            ${ $_[1] } =~ s!</body>!$INSERT</body>!i; 
-            ${ $_[1] } =~ s!</head>!$css</head>!i; 
+            my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
+            if ($$dataref) {
+                if ($contentEncoding eq 'gzip') {
+                    $dest = Compress::Zlib::memGunzip($$dataref) or die "Cannot uncompress: $gzerrno\n";
+                    $dest =~ s!</head>!$css</head>!i; 
+                    $$dataref = $dest . $INSERT;
+                    $$dataref = Compress::Zlib::memGzip($$dataref) or die "Cannot uncompress: $gzerrno\n";
+                } else {
+                    ${ $_[1] } =~ s!</head>!$css</head>!i;
+                    ${ $_[1] } =~ s#$#$INSERT#i;
+                }
+            }
         }
     )
 );
 
 my $filter = HTTP::Proxy::HeaderFilter::simple->new(
     sub { 
-            print Dumper($_[1]);
-
-        if ($_[1]->header('host') eq $hostname) {
-            print "Request for me!\n";
-            print Dumper($_[1]->header('cookie'));
-        }
+            if ($_[1]->header('content-type') =~ m#text/html#i &&  $_[1]->header('content-encoding') =~ /gzip/) {
+                $_[1]->header('content-length', $_[1]->header('content-length') + length($gzip) + length($gzip_css));
+                $contentEncoding = 'gzip';
+            } else {
+                $contentEncoding = '';
+            }
     }
 );
-#$proxy->push_filter( request => $filter );
+$proxy->push_filter( response => $filter );
 
 print "Ready to rumble on " . $proxy->host . ':' . $proxy->port . "\n";
 $proxy->start;
